ba250452721a0ad72e4b6c7fb1cf03732481f79f
Sanskar khandelwal <Sanskarkhandelwal336@gmail.com>
2022-09-22 23:06:47 +0530

    done

--- 03- Unit Converter/script.js
+++ 03- Unit Converter/script.js
@@ -1,1 +1,14 @@
 //  Starting with Unit Converter 
+
+const answer = document.querySelector('.answer');
+
+const section = document.querySelector('section');
+console.log(section);
+
+section.addEventListener('click', function(e){
+  if(e.target.classList.contains('question')){
+    answer.style.display = 'block';
+    console.log("hello I am clicked");
+  }
+}
+   )
